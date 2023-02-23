ef69bac98f9d74e5b09e88e7cef5b2eab40030be
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-24 02:05:58 +0530

    ADDED : Check Equality

--- src/LengthOfLine.java
+++ src/LengthOfLine.java
@@ -22,10 +22,11 @@ public class LengthOfLine {
         System.out.println(y2);
         double LengthOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println(LengthOfLine);
-
-
+        if(x1==x2 && y1==y2) {
+            System.out.println("Length of two line are same");
+            System.out.println("True");
+        }
     }
-
     public static void main(String[] args) {
         length();
     }
